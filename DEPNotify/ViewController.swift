a750faec338c6a043efe1934a8ed2e2489918898
Joel Rennich <joel@orchardandgrove.com>
2017-11-08 20:52:18 -0600

    QuitKey:

--- DEPNotify/ViewController.swift
+++ DEPNotify/ViewController.swift
@@ -14,7 +14,7 @@ private var commandContext = 1
 var background: Background?
 
 class ViewController: NSViewController {
-
+    
     @IBOutlet weak var MainTitle: NSTextField!
     @IBOutlet weak var MainText: NSTextField!
     @IBOutlet weak var ProgressBar: NSProgressIndicator!
@@ -24,28 +24,30 @@ class ViewController: NSViewController {
     @IBOutlet var myView: NSView!
     @IBOutlet weak var helpButton: NSButton!
     @IBOutlet weak var continueButton: NSButton!
-
+    
     var tracker = TrackProgress()
-
+    
     var helpURL = String()
-
+    
     var determinate = false
     var totalItems: Double = 0
     var currentItem = 0
-
+    
     var notify = false
-
+    
     var logo: NSImage?
     var maintextImage: NSImage?
     var notificationImage: NSImage?
-
+    
     var activateEachStep = false
     
     var killCommandFile = false
-
+    
+    var quitKey = "q"
+    
     let myWorkQueue = DispatchQueue(label: "menu.nomad.DEPNotify.background_work_queue", attributes: [])
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Set the background color to white
@@ -53,20 +55,29 @@ class ViewController: NSViewController {
         self.view.layer?.backgroundColor = CGColor.white
         //var isOpaque = false
         ProgressBar.startAnimation(nil)
-
+        
         tracker.addObserver(self, forKeyPath: "statusText", options: .new, context: &statusContext)
         tracker.addObserver(self, forKeyPath: "command", options: .new, context: &commandContext)
         tracker.run()
-
+        
         NSApp.activate(ignoringOtherApps: true)
-
+        
         NSApp.windows[0].makeKeyAndOrderFront(self)
+        
+        NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) {
+            self.flagsChanged(with: $0)
+            return $0
+        }
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
+            self.keyDown(with: $0)
+            return $0
+        }
     }
-
+    
     override func viewDidAppear() {
         //Customize the window's title bar
         let window = self.view.window
-
+        
         if !CommandLine.arguments.contains("-oldskool") {
             window?.styleMask.insert(NSWindowStyleMask.unifiedTitleAndToolbar)
             window?.styleMask.insert(NSWindowStyleMask.fullSizeContentView)
@@ -76,19 +87,19 @@ class ViewController: NSViewController {
             window?.titlebarAppearsTransparent = true
         }
     }
-
+    
     override var representedObject: Any? {
         didSet {
             // Update the view, if already loaded.
         }
     }
-
+    
     func updateStatus(status: String) {
-
+        
         self.StatusText.stringValue = status
         print(self.StatusText.stringValue)
     }
-
+    
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if context == &statusContext {
             if let newValue = change?[.newKey] {
@@ -117,44 +128,44 @@ class ViewController: NSViewController {
             } else {
                 super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
             }
-
+            
         }
     }
-
+    
     func processCommand(command: String) {
-
+        
         switch command.components(separatedBy: " ").first! {
-
+            
         case "Alert:" :
             let alertController = NSAlert()
             alertController.messageText = command.replacingOccurrences(of: "Alert: ", with: "")
             alertController.addButton(withTitle: "Ok")
             alertController.beginSheetModal(for: NSApp.windows[0])
-
+            
         case "Determinate:" :
-
+            
             determinate = true
             ProgressBar.isIndeterminate = false
-
+            
             // default to 1 if we can't make a number
             totalItems = Double(command.replacingOccurrences(of: "Determinate: ", with: "")) ?? 1
             ProgressBar.maxValue = totalItems
             currentItem = 0
             ProgressBar.startAnimation(nil)
-
+            
         case "DeterminateManual:" :
-
+            
             determinate = false
             ProgressBar.isIndeterminate = false
-
+            
             // default to 1 if we can't make a number
             totalItems = Double(command.replacingOccurrences(of: "DeterminateManual: ", with: "")) ?? 1
             ProgressBar.maxValue = totalItems
             currentItem = 0
             ProgressBar.startAnimation(nil)
-
+            
         case "DeterminateManualStep:" :
-
+            
             // default to 1 if we can't make a number
             let stepMove = Int(Double(command.replacingOccurrences(of: "DeterminateManualStep: ", with: "")) ?? 1 )
             currentItem += stepMove
@@ -163,28 +174,28 @@ class ViewController: NSViewController {
                 NSApp.activate(ignoringOtherApps: true)
                 NSApp.windows[0].makeKeyAndOrderFront(self)
             }
-
+            
         case "DeterminateOff:" :
-
+            
             determinate = false
             ProgressBar.isIndeterminate = true
             ProgressBar.stopAnimation(nil)
-
+            
         case "DeterminateOffReset:" :
-
+            
             determinate = false
             currentItem = 0
             ProgressBar.increment(by: -1000)
             ProgressBar.isIndeterminate = true
             ProgressBar.stopAnimation(nil)
-
+            
         case "Help:" :
             helpButton.isHidden = false
             helpURL = command.replacingOccurrences(of: "Help: ", with: "")
-
+            
         case "ContinueButton" :
             continueButton.isHidden = false
-
+            
         case "Image:" :
             logo = NSImage.init(byReferencingFile: command.replacingOccurrences(of: "Image: ", with: ""))
             LogoCell.image = logo
@@ -193,7 +204,7 @@ class ViewController: NSViewController {
             
         case "KillCommandFile:" :
             killCommandFile = true
-
+            
         case "Logout:" :
             let alertController = NSAlert()
             alertController.messageText = command.replacingOccurrences(of: "Logout: ", with: "")
@@ -203,16 +214,16 @@ class ViewController: NSViewController {
                 self.quitSession()
                 NSApp.terminate(self)
             }
-
+            
         case "LogoutNow:":
             self.quitSession()
-
+            
         case "MainText:":
             // Need to do two replacingOccurrences since we are replacing with different values
             let newlinecommand = command.replacingOccurrences(of: "\\n", with: "\n")
             MainText.stringValue = newlinecommand.replacingOccurrences(of: "MainText: ", with: "")
             ImageCell.image = NSImage.init(byReferencingFile: "")
-
+            
         case "MainTextImage:" :
             maintextImage = NSImage.init(byReferencingFile: command.replacingOccurrences(of: "MainTextImage: ", with: ""))
             ImageCell.image = maintextImage
@@ -220,22 +231,22 @@ class ViewController: NSViewController {
             ImageCell.imageAlignment = .alignCenter
             MainText.stringValue = ""
             MainTitle.stringValue = ""
-
+            
         case "MainTitle:" :
             // Need to do two replacingOccurrences since we are replacing with different values
             let newlinecommand = command.replacingOccurrences(of: "\\n", with: "\n")
             MainTitle.stringValue = newlinecommand.replacingOccurrences(of: "MainTitle: ", with: "")
             ImageCell.image = NSImage.init(byReferencingFile: "")
-
+            
         case "Notification:" :
             sendNotification(text: command.replacingOccurrences(of: "Notification: ", with: ""))
-
+            
         case "NotificationImage:" :
             notificationImage = NSImage.init(byReferencingFile: command.replacingOccurrences(of: "NotificationImage: ", with: ""))
-
+            
         case "NotificationOn:" :
             notify = true
-
+            
         case "WindowStyle:" :
             switch command.replacingOccurrences(of: "WindowStyle: ", with: "") {
             case "Activate" :
@@ -259,14 +270,14 @@ class ViewController: NSViewController {
             default :
                 break
             }
-
+            
         case "WindowTitle:" :
             let title = command.replacingOccurrences(of: "WindowTitle: ", with: "")
             NSApp.windows[0].title = title
-
+            
         case "Quit" :
             NSApp.terminate(self)
-
+            
         case "Quit:" :
             let alertController = NSAlert()
             alertController.messageText = command.replacingOccurrences(of: "Quit: ", with: "")
@@ -274,7 +285,14 @@ class ViewController: NSViewController {
             alertController.beginSheetModal(for: NSApp.windows[0]) { response in
                 NSApp.terminate(self)
             }
-
+            
+        case "QuitKey:" :
+            let quitKeyTemp = command.replacingOccurrences(of: "QuitKey: ", with: "")
+            
+            if quitKeyTemp.count == 1 {
+                quitKey = quitKeyTemp
+            }
+            
         case "Restart:" :
             let alertController = NSAlert()
             alertController.messageText = command.replacingOccurrences(of: "Restart: ", with: "")
@@ -284,28 +302,28 @@ class ViewController: NSViewController {
                 self.reboot()
                 NSApp.terminate(self)
             }
-
+            
         case "RestartNow:" :
             self.reboot()
-
+            
         default:
             break
         }
     }
-
+    
     func quitSession() {
         var targetDesc: AEAddressDesc = AEAddressDesc.init()
         var psn = ProcessSerialNumber(highLongOfPSN: UInt32(0), lowLongOfPSN: UInt32(kSystemProcess))
         var eventReply: AppleEvent = AppleEvent(descriptorType: UInt32(typeNull), dataHandle: nil)
         var eventToSend: AppleEvent = AppleEvent(descriptorType: UInt32(typeNull), dataHandle: nil)
-
+        
         var status: OSErr = AECreateDesc(
             UInt32(typeProcessSerialNumber),
             &psn,
             MemoryLayout<ProcessSerialNumber>.size,
             &targetDesc
         )
-
+        
         status = AECreateAppleEvent(
             UInt32(kCoreEventClass),
             kAELogOut,
@@ -314,9 +332,9 @@ class ViewController: NSViewController {
             AETransactionID(kAnyTransactionID),
             &eventToSend
         )
-
+        
         AEDisposeDesc(&targetDesc)
-
+        
         let osstatus = AESendMessage(
             &eventToSend,
             &eventReply,
@@ -327,22 +345,22 @@ class ViewController: NSViewController {
         if killCommandFile {
             tracker.killCommandFile()
         }
-
+        
     }
-
+    
     func reboot() {
         var targetDesc: AEAddressDesc = AEAddressDesc.init()
         var psn = ProcessSerialNumber(highLongOfPSN: UInt32(0), lowLongOfPSN: UInt32(kSystemProcess))
         var eventReply: AppleEvent = AppleEvent(descriptorType: UInt32(typeNull), dataHandle: nil)
         var eventToSend: AppleEvent = AppleEvent(descriptorType: UInt32(typeNull), dataHandle: nil)
-
+        
         var status: OSErr = AECreateDesc(
             UInt32(typeProcessSerialNumber),
             &psn,
             MemoryLayout<ProcessSerialNumber>.size,
             &targetDesc
         )
-
+        
         status = AECreateAppleEvent(
             UInt32(kCoreEventClass),
             kAERestart,
@@ -351,9 +369,9 @@ class ViewController: NSViewController {
             AETransactionID(kAnyTransactionID),
             &eventToSend
         )
-
+        
         AEDisposeDesc(&targetDesc)
-
+        
         let osstatus = AESendMessage(
             &eventToSend,
             &eventReply,
@@ -366,14 +384,14 @@ class ViewController: NSViewController {
         }
         
     }
-
+    
     func sendNotification(text: String) {
         let notification = NSUserNotification()
-
+        
         if logo != nil {
             notification.contentImage = logo
         }
-
+        
         notification.title = "Setup notification"
         notification.informativeText = text
         notification.soundName = NSUserNotificationDefaultSoundName
@@ -382,14 +400,24 @@ class ViewController: NSViewController {
     @IBAction func HelpClick(_ sender: Any) {
         NSWorkspace.shared().open(URL(string: helpURL)!)
     }
-
+    
     @IBAction func continueButton(_ sender: Any) {
         let fileMgr = FileManager()
         let pathDone = "/Users/Shared/.DEPNotifyDone"
         fileMgr.createFile(atPath: pathDone, contents: nil, attributes: nil)
         NSApp.terminate(self)
     }
-
+    
+    // Key pressing
+    
+    override func keyDown(with event: NSEvent) {
+        switch event.modifierFlags.intersection(.deviceIndependentFlagsMask) {
+        case [.command, .control] where event.charactersIgnoringModifiers == quitKey:
+            NSApp.terminate(nil)
+        default:
+            break
+        }
+    }
 }
 
 class WindowController: NSWindowController {
@@ -397,16 +425,16 @@ class WindowController: NSWindowController {
     override func windowDidLoad() {
         
         if CommandLine.arguments.contains("-fullScreen") {
-        
-        NSApp.activate(ignoringOtherApps: true)
-        self.window?.makeKeyAndOrderFront(self)
-        self.window?.center()
-        self.window?.isMovable = false
-        
-        background = storyboard?.instantiateController(withIdentifier: "Background") as? Background
-        background?.showWindow(self)
-        background?.sendBack()
-        NSApp.windows[0].level = Int(CGWindowLevelForKey(.maximumWindow))
-    }
+            
+            NSApp.activate(ignoringOtherApps: true)
+            self.window?.makeKeyAndOrderFront(self)
+            self.window?.center()
+            self.window?.isMovable = false
+            
+            background = storyboard?.instantiateController(withIdentifier: "Background") as? Background
+            background?.showWindow(self)
+            background?.sendBack()
+            NSApp.windows[0].level = Int(CGWindowLevelForKey(.maximumWindow))
+        }
     }
 }
